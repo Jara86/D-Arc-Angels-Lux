9a09ccb5544b3bbec94d35c9855d24f5b48eb570
Jara86 <jarouschka@gmail.com>
2025-01-31 01:13:30 +0100

    update forme menber

--- member.js
+++ member.js
@@ -82,7 +82,7 @@ document.getElementById('membershipForm').addEventListener('submit', function(e)
     submitButton.textContent = 'Sending...';
     submitButton.disabled = true;
 
-    fetch('https://formsubmit.co/cdf96776353aec10207def6412ff3021 ', {
+    fetch('https://formsubmit.co/jarouschka@gmail.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
